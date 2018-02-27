02cdbcd80f4e0325187ade379942728467b0280b
唯ぃ一世 <1285913468@qq.com>
2018-02-27 17:40:01 +0800

    Initial commit

---
+++ src/main/java/cn/edu/cuit/cuitapp/CuitappApplication.java
@@ -0,0 +1,12 @@
+package cn.edu.cuit.cuitapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CuitappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CuitappApplication.class, args);
+	}
+}
